c756c5e9420bc12ec031e7b0201a8915fecd539c
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-11-24 14:06:47 +0100

    add about add rules

--- ganopoly-deal/src/app/app.routes.ts
+++ ganopoly-deal/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from '../game/game';
+import { RulesComponent } from '../rules/rules';
+import { AboutComponent } from '../about/about';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'game', pathMatch: 'full' }, // Route par défaut
   { path: 'game', component: GameComponent },
-  // tu peux ajouter d'autres routes plus tard, ex:
-  // { path: 'settings', component: SettingsComponent }
+  { path: 'rules', component: RulesComponent },
+  { path: 'about', component: AboutComponent },
 ];
--- ganopoly-deal/src/app/app.ts
+++ ganopoly-deal/src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GameComponent } from "../game/game";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GameComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('ganopoly-deal');
+  protected readonly title = signal('Ganopoly Deal');
 }
