c875d6a2f9d1c84c1de3d8eddb8c748b67924afd
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-11-24 13:57:10 +0100

    add game component

--- ganopoly-deal/src/app/app.routes.ts
+++ ganopoly-deal/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Game } from '../game/game';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'game', pathMatch: 'full' }, // Route par défaut
+  { path: 'game', component: Game },
+  // tu peux ajouter d'autres routes plus tard, ex:
+  // { path: 'settings', component: SettingsComponent }
+];
