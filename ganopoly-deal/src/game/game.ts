4bc59410f2e942564403cdfb0d21841ad4bdfe8a
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-11-24 14:50:53 +0100

    add card model

--- ganopoly-deal/src/game/game.ts
+++ ganopoly-deal/src/game/game.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Card, DealType, DealColor, DealAction } from '../models/card';
 
 @Component({
   selector: 'app-game',
@@ -7,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './game.scss',
 })
 export class GameComponent {
+  cards: Card[] = [];
 
+  private generateDeck(): Card[] {
+    const deck = [];
+    deck.push(new Card(DealType.property, null, DealColor.brown, 1, 1, 2));
+    deck.push(new Card(DealType.property, null, DealColor.lightBlue, 2, 2, 4));
+    deck.push(new Card(DealType.notes, DealAction.rent, DealColor.lightBlue, 3, 0, 3));
+    deck.push(new Card(DealType.notes, DealAction.doubleRent, 0, 0, 0));
+    return deck;
+  }
+
+  initializeCards() {
+    this.cards = this.generateDeck();
+  }
+
+
+  newGame() {
+    this.initializeCards();
+    console.log('New Game');
+    console.log(this.cards);
+  }
+
+  constructor() {
+
+  }
 }
---
+++ ganopoly-deal/src/models/card.ts
@@ -0,0 +1,43 @@
+export  enum DealColor {
+  brown,
+  lightBlue,
+  pink,
+  orange,
+  red,
+  yellow,
+  green,
+  darkBlue
+}
+
+export  enum DealAction {
+  fight,
+  swap,
+  banco,
+  joker,
+  jackpot,
+  birthday,
+  multicolor,
+  rent,
+  doubleRent,
+  start,
+  house,
+  hotel
+}
+
+export enum DealType {
+  property,
+  property_joker,
+  propert_player,
+  notes
+}
+
+export class Card {
+  constructor(
+    public cardType: DealType = DealType.property,
+    public cardAction: DealAction | null = null,
+    public color: DealColor = DealColor.blue,
+    public value: number = 0,
+    public rent: number = 0,
+    public total: number = 0
+  ) {}
+}
